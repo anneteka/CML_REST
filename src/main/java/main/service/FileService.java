f6adb4b1e7007792daef81848c454eb13665a764
Anna <a.karlysheva@ukma.edu.ua>
2020-08-25 23:29:13 +0300

    base for controller and service classes

--- src/main/java/main/service/FileService.java
+++ src/main/java/main/service/FileService.java
@@ -1,7 +1,43 @@
 package main.service;
 
+import main.repository.FileRepository;
+import main.repository.document.File;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FileService {
+    private FileRepository fileRepository;
+
+    @Autowired
+    public FileService(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    public List<File> findAll() {
+        return fileRepository.findAll();
+    }
+
+
+    public String save(File file){
+        return "id";
+    }
+
+    public void deleteFile(String id){
+
+    }
+
+    public void addTags(){
+
+    }
+    public void deleteTags(){
+
+    }
+
+    public List<File> listFiles(){
+
+        return null;
+    }
 }
