90e4d643b3a15b7b425bbf60688bed5cb5ac9f35
Anna <a.karlysheva@ukma.edu.ua>
2020-08-27 01:04:31 +0300

    forgot to add class to git

---
+++ src/main/java/main/util/FileUtil.java
@@ -0,0 +1,21 @@
+package main.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import main.repository.document.File;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+    public static ArrayList<File> intersect(List<File> main, List<File> containing){
+        ArrayList<File> res = new ArrayList<>();
+        for (File f: main){
+            if (containing.contains(f)){
+                res.add(f);
+            }
+        }
+        return res;
+    }
+
+
+}
