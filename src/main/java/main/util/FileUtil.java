259a443f14f89e4ca53ffdf9e2b576d29dd8f06e
Anna <a.karlysheva@ukma.edu.ua>
2020-08-29 16:48:02 +0300

    remove unnecessary code

--- src/main/java/main/util/FileUtil.java
+++ src/main/java/main/util/FileUtil.java
@@ -1,23 +1,4 @@
 package main.util;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import main.repository.document.File;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class FileUtil {
-    // return only those elements that are present in both lists
-    //todo with query
-    public static ArrayList<File> intersect(List<File> main, List<File> containing){
-        ArrayList<File> res = new ArrayList<>();
-        for (File f: main){
-            if (containing.contains(f)){
-                res.add(f);
-            }
-        }
-        return res;
-    }
-
-
 }
--- src/test/java/main/controller/FileUtilTest.java
+++ src/test/java/main/controller/FileUtilTest.java
@@ -13,24 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FileUtilTest {
     @Test
     public void simpleTest() throws JsonProcessingException {
-        ArrayList<File> array = new ArrayList<>();
-        ArrayList<File> array2 = new ArrayList<>();
-        File f1 = new File();
-        File f2 = new File();
-        File f3 = new File();
-        File f4 = new File();
-        f1.setId("id1");
-        f2.setId("id2");
-        f3.setId("id3");
-        f4.setId("id4");
-        array.add(f1);
-        array.add(f2);
-        array.add(f3);
-        array2.add(f2);
-        array2.add(f3);
-        array2.add(f4);
-        File[] expected = {f2,f3};
-        assertArrayEquals(expected,FileUtil.intersect(array,array2).toArray());
+
     }
 
 }
